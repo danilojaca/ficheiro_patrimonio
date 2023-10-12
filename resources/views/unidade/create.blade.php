fb0b64f9e859ca6e7a94a66e42de73278857c022
danilojaca <danilomontei@gmail.com>
2023-10-12 16:21:35 +0100

    Ficheiro 2.35

--- resources/views/unidade/create.blade.php
+++ resources/views/unidade/create.blade.php
@@ -9,48 +9,46 @@
             </div>
             <ul class="navbar-nav">      
                 <li class="nav-item">
-                @can('role-create')
                     <a class="btn btn-primary" href="{{ route('unidade.index') }}" data-bs-toggle="tooltip" data-bs-placement="top" title="Voltar"><i class="bi bi-reply-fill"></i></a>
-                @endcan
                 </li>            
             </ul>
         </div>
     </nav>
 </div>
 <div class="container">
-         <form class="row g-3" action="{{route('unidade.store')}}" method='POST' >
-        @csrf
-    <div class="col-md-3">
-    </div>
-  <div class="col-md-3">    
-    <label  class="form-label">{{"Edificio"}}</label>
-    <select class="form-select @error('edificio_id') is-invalid @enderror" name="edificio_id" aria-label="Default select example">
-        <option data-default disabled selected>{{"Selecione o Edificio"}}</option>
-        @foreach ($edificios as $edificio )
-           <option value="{{$edificio->id}}" {{ old('edificio_id') ? 'selected':''}}>{{$edificio->edificio}}</option> 
-        @endforeach
-    </select>
-    @error('edificio_id')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-    @enderror
-  </div>
-  <div class="col-md-3">
-    <label  class="form-label">{{"Unidade"}}</label>
-    <input type="text" name='unidade' class="form-control @error('unidade') is-invalid @enderror" value="{{ old('unidade')}}" >
-    @error('unidade')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-    @enderror
-  </div>
-  
-  
-  <div class="col-md-1 pt-4 mt-4">
-    <button type="submit" class="btn btn-primary">{{"Cadastrar"}}</button>
-     
-  </div>
-</form>
- </div>
+    <form class="row g-3" action="{{route('unidade.store')}}" method='POST' >
+            @csrf
+        <div class="col-md-6 offset-md-1">    
+                <label  class="form-label">{{"Edificio"}}</label>
+                <select class="form-select @error('edificio_id') is-invalid @enderror" name="edificio_id" data-placeholder="Selecione o Edificio" id="edificio_id">
+                        <option data-default disabled selected></option>
+                    @foreach ($edificios as $edificio )
+                        <option value="{{$edificio->id}}" {{ old('edificio_id') ? 'selected':''}}>{{$edificio->edificio}}</option> 
+                    @endforeach
+                </select>
+            @error('edificio_id')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="col-md-3">
+                <label  class="form-label">{{"Unidade"}}</label>
+                <input type="text" name='unidade' class="form-control bg-white @error('unidade') is-invalid @enderror" value="{{ old('unidade')}}" >
+            @error('unidade')
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="col-md-1 pt-4 mt-4">
+            <button type="submit" class="btn btn-primary">{{"Cadastrar"}}</button>
+        </div>
+    </form>
+</div>
+<script>
+$( '#edificio_id' ).select2( {
+    theme: 'bootstrap-5'
+} );
+</script>
 @endsection
--- resources/views/unidade/edit.blade.php
+++ resources/views/unidade/edit.blade.php
@@ -9,48 +9,48 @@
             </div>
             <ul class="navbar-nav">      
                 <li class="nav-item">
-                @can("role-create")
                     <a class="btn btn-primary" href="{{ route("unidade.index") }}" data-bs-toggle="tooltip" data-bs-placement="top" title="Voltar"><i class="bi bi-reply-fill"></i></a>
-                @endcan
                 </li>            
             </ul>
         </div>
     </nav>
 </div>
 <div class="container">
-        <form  class="row g-3" action={{route("unidade.update", ["unidade" => $unidade->id])}} method="POST" >
-        @method("PUT")
-         @csrf 
-    <div class="col-md-3">
-    </div>
-  <div class="col-md-3">    
-    <label  class="form-label">{{"Edificio"}}</label>
-    <select class="form-select @error("edificio_id") is-invalid @enderror" name="edificio_id" aria-label="Default select example">
-        <option data-default disabled selected>{{"Selecione o Edificio"}}</option>
-        @foreach ($edificios as $edificio )
-           <option value="{{$edificio->id}}" {{$edificio->id == $unidade->edificio_id ? "selected" : ""}}>{{$edificio->edificio}}</option> 
-        @endforeach
-    </select>
-    @error("edificio_id")
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-    @enderror
-  </div>
-  <div class="col-md-3">
-    <label  class="form-label">{{"Unidade"}}</label>
-    <input type="text" name="unidade" class="form-control @error("unidade") is-invalid @enderror" value="{{ $unidade->unidade ?? old("unidade")}}" >
-    @error("unidade")
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-    @enderror
-  </div>
-  
-  
-  <div class="col-md-1 pt-4 mt-4">
-    <button type="submit" class="btn btn-primary">{{"Editar"}}</button>
-  </div>
-</form>
- </div>
+    <form  class="row g-3" action={{route("unidade.update", ["unidade" => $unidade->id])}} method="POST" >
+    @method("PUT")
+    @csrf 
+        <div class="col-md-6 offset-md-1">    
+                <label  class="form-label">{{"Edificio"}}</label>
+                <select class="form-select @error("edificio_id") is-invalid @enderror" name="edificio_id"  id="edificio_id" aria-label="Default select example">
+                        <option data-default disabled selected>{{"Selecione o Edificio"}}</option>
+                    @foreach ($edificios as $edificio )
+                        <option value="{{$edificio->id}}" {{$edificio->id == $unidade->edificio_id ? "selected" : ""}}>{{$edificio->edificio}}</option> 
+                    @endforeach
+                </select>
+            @error("edificio_id")
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="col-md-3">
+                <label  class="form-label">{{"Unidade"}}</label>
+                <input type="text" name="unidade" class="form-control bg-white @error("unidade") is-invalid @enderror" value="{{ $unidade->unidade ?? old("unidade")}}" >
+            @error("unidade")
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $message }}</strong>
+                </span>
+            @enderror
+        </div>
+        <div class="col-md-1 pt-4 mt-4">
+                <button type="submit" class="btn btn-primary">{{"Editar"}}</button>
+        </div>
+    </form>
+</div>
+ 
+<script>
+$( '#edificio_id' ).select2( {
+    theme: 'bootstrap-5'
+} );
+</script>
 @endsection
